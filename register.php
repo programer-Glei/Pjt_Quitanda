cd614717a8abb35e5354c4828801292e2dba80e0
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-15 22:53:36 -0300

    proximo comit

--- register.php
+++ register.php
@@ -7,10 +7,15 @@
     <link rel="stylesheet" href="scss/estilo.css">
 
     <!-- font awesome cdn link -->
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <title>Cadastro & Login</title>
 </head>
 <body>
-    
+    <section class="form-container">
+        <form action="" enctype="multipart/form-data" method="POST">
+            <h3>Cadastrar</h3>
+            <input type="text">
+        </form>
+    </section>
 </body>
 </html>
