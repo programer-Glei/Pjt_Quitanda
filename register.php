7c19b335e9b7d257f34d9849bf5fcfc7767e90e0
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-16 15:09:36 -0300

    proximo comit

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="scss/estilo.css">
+    <link rel="stylesheet" href="css/estilo.css">
 
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -14,7 +14,12 @@
     <section class="form-container">
         <form action="" enctype="multipart/form-data" method="POST">
             <h3>Cadastrar</h3>
-            <input type="text">
+            <input type="text" name="name" class="box" placeholder="Digite seu nome" required>
+            <input type="email" name="email" class="box" placeholder="Digite seu email" required>
+            <input type="password" name="pass" class="box" placeholder="Digite sua senha" required>
+            <input type="password" name="cpass" class="box" placeholder="confirme sua senha" required>
+            <input type="file" name="pass" class="image" accept="image/jpg, image/jpeg, image/png" required>
+            <input type="submit" value="Cadastrar agora" class="btn" name="submit">
         </form>
     </section>
 </body>
