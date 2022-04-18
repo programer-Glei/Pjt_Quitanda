9bed6f53b71d960c1d68ce5a80f223f6bc90ca31
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-18 20:23:01 -0300

    proximo comit

--- admin_page.php
+++ admin_page.php
@@ -19,10 +19,12 @@ if(!isset($admin_id)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo.css">
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <title>Página de administração</title>
 </head>
 <body>
-    <h1>Página de administração</h1>
-    <a href="logout.php">Sair</a>
+    <?php include 'admin_header.php'; ?>
 </body>
 </html>
