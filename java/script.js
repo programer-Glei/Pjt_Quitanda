db7bb496acd36eb75b5d35d8693fc648bcd31a09
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-19 23:31:07 -0300

    proximo comit

---
+++ java/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    
+}
