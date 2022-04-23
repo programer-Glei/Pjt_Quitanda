6dec3cb14410400f7b1c277cd541f91ac6dba5a9
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-23 19:55:53 -0300

    proximo comit

--- admin_header.php
+++ admin_header.php
@@ -36,6 +36,11 @@ if(isset($message)){
             <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
             <p><?= $fetch_profile['name']; ?></p>
             <a href="admin_update_profile.php" class="btn">Atualizar perfil</a>
+            <a href="logout.php" class="delete-btn">Sair</a>
+            <div class="flex-btn">
+                <a href="login.php" class="option-btn">Entrar</a>
+                <a href="register.php" class="option-btn">Cadastrar</a>
+            </div>
         </div>
     </div>
 </header>
