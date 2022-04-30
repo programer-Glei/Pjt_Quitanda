dbdecffa6262b3b809db2ca3900cd449d5e2fca2
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-04-30 19:26:43 -0300

    proximo comit

---
+++ admin_update_profile.php
@@ -0,0 +1,37 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+    header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Atualizar perfil</title>
+</head>
+<body>
+    <?php include 'admin_header.php'; ?>
+
+    <section class="update-profile">
+        <h1 class="title">Atualizar perfil</h1>
+    </section>
+
+    <script src="java/script.js"></script>
+</body>
+</html>
+
