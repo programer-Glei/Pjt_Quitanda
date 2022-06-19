e808fcd5e7f893eb8c4ca63c61d8669ae754943c
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-06-19 16:09:45 -0300

    proximo comit

--- admin_update_products.php
+++ admin_update_products.php
@@ -27,20 +27,38 @@ if(!isset($admin_id)){
 <body>
     <?php include 'admin_header.php'; ?>
 
-    <div class="update-products">
-        <p class="empty">nenhum produto encontrado!</p>
+    <section class="update-products">
+
+        <h1 class="title">Atualizar produto</h1>
 
         <?php
             $update_id = $_GET['update'];
             $select_products = $conn->prepare("SELECT * FROM `products` WHERE id = ?");
             $select_products->execute([$update_id]);
             if($select_products->rowCount() > 0){
-
+                while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+        ?>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+            <input type="text" name="name" placeholder="Digitar nome" required class="box" value="<?= $fetch_products['name']; ?>">
+            <input type="number" name="price" placeholder="Digitar preço" required class="box" value="<?= $fetch_products['price']; ?>">
+            <select name="category" class="box" required>
+                <option selected><?= $fetch_products['category']; ?></option>
+                <option value="vegitbales">Vegetais</option>
+                <option value="fruits">Frutas</option>
+                <option value="meat">Carne</option>
+                <option value="fish">Peixe</option>
+            </select>
+            <textarea name="details" required class="box" placeholder="Descrição do produto" cols="30" rows="10"><?= $fetch_products['details']; ?></textarea>
+            <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png">
+        </form>
+        <?php
+             }
             }else{
-                
+                echo '<p class="empty">nenhum produto encontrado!</p>';
             }
         ?>
-    </div>
+    </section>
 
     <script src="java/script.js"></script>
 </body>
