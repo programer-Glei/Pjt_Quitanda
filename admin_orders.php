dffcb1e8c64fc1352193646925f142cc750adeba
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-06-25 19:22:06 -0300

    proximo comit

--- admin_orders.php
+++ admin_orders.php
@@ -27,6 +27,25 @@ if(!isset($admin_id)){
 <body>
     <?php include 'admin_header.php'; ?>
 
+    <section class="placed-orders">
+        <div class="box-container">
+            <?php
+                $select_orders = $conn->prepare("SELECT * FROM `orders`");
+                $select_orders->execute();
+                if($select_orders->rowCount() > 0){
+                    while($fetch_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
+                        
+                    }
+                }else{
+                    echo '<p class="empty">nenhum pedido feito ainda!</p>';
+                }
+            ?>
+            <div class="box">
+
+            </div>
+        </div>
+    </section>
+
     <script src="java/script.js"></script>
 </body>
 </html>
