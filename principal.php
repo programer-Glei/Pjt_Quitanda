85be4232b05b524db926eca9fef90f96ef5f6489
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-07-17 15:47:06 -0300

    proximo comit

--- principal.php
+++ principal.php
@@ -12,6 +12,8 @@ if(!isset($user_id)){
 
 if(isset($_POST['add_to_wishlist'])){
 
+    $pid = $_POST['pid'];
+    $pid = filter_var($pid, FILTER_SANITIZE_STRING);
     $p_name = $_POST['p_name'];
     $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
     $p_price = $_POST['p_price'];
@@ -29,7 +31,48 @@ if(isset($_POST['add_to_wishlist'])){
         $message[] = 'já adicionado à lista de desejos';
     }elseif($check_cart_number->rowCount() > 0){
         $message[] = 'já adicionado ao carrinho';
+    }else{
+        $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
+        $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
+        $message[] = 'adicionado a lista de desejos';
     }
+}
+
+if(isset($_POST['add_to_cart'])){
+
+    $pid = $_POST['pid'];
+    $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+    $p_name = $_POST['p_name'];
+    $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+    $p_price = $_POST['p_price'];
+    $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+    $p_image = $_POST['p_image'];
+    $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+    $p_qty = $_POST['p_qty'];
+    $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+
+    $check_cart_number = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+    $check_cart_number->execute([$p_name,$user_id]);
+
+    
+    if($check_cart_number->rowCount() > 0){
+        $message[] = 'já adicionado ao carrinho';
+    }else{
+
+        $check_wishlist_number = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+        $check_wishlist_number->execute([$p_name,$user_id]);
+
+        if($check_wishlist_number->rowCount() > 0){
+            $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+            $delete_wishlist->execute([$p_name,$user_id]);
+        }
+
+        $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+        $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
+        $message[] = 'adicionado ao carrinho!';
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -98,7 +141,7 @@ if(isset($_POST['add_to_wishlist'])){
                 <a href="view_page.php?pid=<?= $fetch_products['id']; ?>"  class="fas fa-eye"></a>
                 <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
                 <div class="name"><?= $fetch_products['name']; ?></div>
-                <input type="hidden" name="p_id" value="<?= $fetch_products['id']; ?>">
+                <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
                 <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
                 <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
                 <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
