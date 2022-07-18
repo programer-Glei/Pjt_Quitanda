c72dd1c752d100b30f453e4d937a044c57dfab00
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-07-18 20:09:08 -0300

    proximo comit

--- shop.php
+++ shop.php
@@ -25,6 +25,36 @@ if(!isset($user_id)){
 </head>
 <body>
     <?php include 'header.php'; ?>
+    <section class="products">
+        <h1 class="title">Produtos mais recentes</h1>
+        <div class="box-container">
+            <?php
+                $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+                $select_products->execute();
+                if($select_products->rowCount() > 0){
+                    while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+            ?>
+            <form action="" class="box" method="POST">
+                <div class="price">R$<?= $fetch_products['price']; ?></div>
+                <a href="view_page.php?pid=<?= $fetch_products['id']; ?>"  class="fas fa-eye"></a>
+                <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+                <div class="name"><?= $fetch_products['name']; ?></div>
+                <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+                <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
+                <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
+                <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+                <input type="number" name="p_qty" value="1" min="1" class="qty">
+                <input type="submit" value="Adicionar a lista de desejos" class="option-btn" name="add_to_wishlist">
+                <input type="submit" value="Adicionar no carrinho" class="btn" name="add_to_cart">
+            </form>
+            <?php
+                }
+            }else{
+                echo '<p class="empty">Nenhum produto adicionado!</p>';
+            }
+            ?>
+        </div>
+    </section>
     <?php include 'footer.php'; ?>
     <script src="java/script.js"></script>
 </body>
