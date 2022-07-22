ad32611623709bab0a43f27de00935a84067f36e
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-07-22 20:33:00 -0300

    proximo comit

--- contact.php
+++ contact.php
@@ -10,6 +10,26 @@ if(!isset($user_id)){
     header('location:login.php');
 }
 
+if(isset($_POST['send'])){
+
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = $_POST['email'];
+    $email = filter_var($email, FILTER_SANITIZE_STRING);
+    $number = $_POST['number'];
+    $number = filter_var($number, FILTER_SANITIZE_STRING);
+    $msg = $_POST['msg'];
+    $msg = filter_var($msg, FILTER_SANITIZE_STRING);
+
+    $select_message = $conn->prepare("SELECT * FROM `message` WHERE name = ?, email = ?, number = ?, message = ?");
+    $select_message->execute([$name,$email,$number,$msg]);
+
+    if($select_message->rowCount() > 0){
+        
+    }
+
+}
+
 ?>
 
 <!DOCTYPE html>
