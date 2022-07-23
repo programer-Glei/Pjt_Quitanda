50090c327b5502d2f0e4f1a0d0ca92ce192ea45f
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-07-23 20:09:20 -0300

    proximo comit

--- search_page.php
+++ search_page.php
@@ -10,6 +10,68 @@ if(!isset($user_id)){
     header('location:login.php');
 }
 
+if(isset($_POST['add_to_wishlist'])){
+
+    $pid = $_POST['pid'];
+    $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+    $p_name = $_POST['p_name'];
+    $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+    $p_price = $_POST['p_price'];
+    $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+    $p_image = $_POST['p_image'];
+    $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+
+    $check_wishlist_number = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+    $check_wishlist_number->execute([$p_name,$user_id]);
+
+    $check_cart_number = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+    $check_cart_number->execute([$p_name,$user_id]);
+
+    if($check_wishlist_number->rowCount() > 0){
+        $message[] = 'já adicionado à lista de desejos';
+    }elseif($check_cart_number->rowCount() > 0){
+        $message[] = 'já adicionado ao carrinho';
+    }else{
+        $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
+        $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
+        $message[] = 'adicionado a lista de desejos';
+    }
+}
+
+if(isset($_POST['add_to_cart'])){
+
+    $pid = $_POST['pid'];
+    $pid = filter_var($pid, FILTER_SANITIZE_STRING);
+    $p_name = $_POST['p_name'];
+    $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
+    $p_price = $_POST['p_price'];
+    $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
+    $p_image = $_POST['p_image'];
+    $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+    $p_qty = $_POST['p_qty'];
+    $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+
+    $check_cart_number = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+    $check_cart_number->execute([$p_name,$user_id]);
+
+    
+    if($check_cart_number->rowCount() > 0){
+        $message[] = 'já adicionado ao carrinho';
+    }else{
+
+        $check_wishlist_number = $conn->prepare("SELECT * FROM `wishlist` WHERE name = ? AND user_id = ?");
+        $check_wishlist_number->execute([$p_name,$user_id]);
+
+        if($check_wishlist_number->rowCount() > 0){
+            $delete_wishlist = $conn->prepare("DELETE FROM `wishlist` WHERE name = ? AND user_id = ?");
+            $delete_wishlist->execute([$p_name,$user_id]);
+        }
+
+        $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+        $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
+        $message[] = 'adicionado ao carrinho!';
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +92,42 @@ if(!isset($user_id)){
             <input type="text" class="box" name="search_box" placeholder="procurar produtos...">
             <input type="submit" name="search_btn" value="Procurar" class="btn">
         </form>
+    </section>
+
+    <?php
+        if(isset($_POST['search_btn'])){
+            
+        }
+    ?>
+    <section class="products">
+        <h1 class="title">Produtos mais recentes</h1>
+        <div class="box-container">
+            <?php
+                $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+                $select_products->execute();
+                if($select_products->rowCount() > 0){
+                    while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+            ?>
+            <form action="" class="box" method="POST">
+                <div class="price">R$<?= $fetch_products['price']; ?></div>
+                <a href="view_page.php?pid=<?= $fetch_products['id']; ?>"  class="fas fa-eye"></a>
+                <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+                <div class="name"><?= $fetch_products['name']; ?></div>
+                <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+                <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
+                <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
+                <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+                <input type="number" name="p_qty" value="1" min="1" class="qty">
+                <input type="submit" value="Adicionar a lista de desejos" class="option-btn" name="add_to_wishlist">
+                <input type="submit" value="Adicionar no carrinho" class="btn" name="add_to_cart">
+            </form>
+            <?php
+                }
+            }else{
+                echo '<p class="empty">Nenhum produto adicionado!</p>';
+            }
+            ?>
+        </div>
     </section>
     <?php include 'footer.php'; ?>
     <script src="java/script.js"></script>
